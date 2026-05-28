dee7a6fea3b4f92b02d036eed84836f47d6e7c61
erzan <do.earljan@gmail.com>
2026-05-28 18:14:47 +0800

    feat: get product list api added

---
+++ src/main/java/com/erzan/task_api/controller/ProductController.java
@@ -0,0 +1,27 @@
+package com.erzan.task_api.controller;
+
+import com.erzan.task_api.entity.Product;
+import com.erzan.task_api.service.ProductService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public List<Product> getProducts() {
+        return productService.getProducts();
+    }
+
+    
+}
