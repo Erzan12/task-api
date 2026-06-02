9ff6497e597fbf2018eb0a66400532c6c58bf87d
erzan12 <do.earljan@gmail.com>
2026-06-02 16:46:37 +0800

    feat: added new dto api response for product list

---
+++ src/main/java/com/erzan/task_api/dto/api_response/list_response/ProductResponse.java
@@ -0,0 +1,11 @@
+package com.erzan.task_api.dto.api_response.list_response;
+
+import java.time.LocalDateTime;
+
+public record ProductResponse(
+        Long id,
+        String name,
+        String expiry,
+        String sellerName,
+        LocalDateTime createdAt
+) {}
