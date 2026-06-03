4085762325d81421707344291f5e57d07aeef7cb
erzan12 <do.earljan@gmail.com>
2026-06-03 09:56:42 +0800

    refactor: added product color attribute in product response dto

--- src/main/java/com/erzan/task_api/dto/api_response/list_response/ProductResponse.java
+++ src/main/java/com/erzan/task_api/dto/api_response/list_response/ProductResponse.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 public record ProductResponse(
         Long id,
         String name,
+        String color,
         String expiry,
         String sellerName,
         LocalDateTime createdAt
